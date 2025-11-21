926b64e323ba54ff4a34164cfe7beff0ea38271b
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-11-22 03:20:09 +0900

    chore: 프로젝트 초기 설정 (Axios, Babel, ESLint, EAS)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,41 @@
 module.exports = {
-  extends: ['universe/native'],
+  root: true,
+  extends: ['universe/native'], // Expo 공식 설정 사용
+
+  // ★ 아까 에러의 원인! 플러그인을 명시해줘야 규칙을 찾습니다.
+  plugins: ['react-native', 'react', 'import'],
+
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    // 1. 콘솔 로그: 개발 중엔 허용 (나중에 배포할 때만 error로 바꾸세요)
+    'no-console': 'off',
+
+    // 2. 인라인 스타일: 간단한 건 써도 됨. (너무 길어지면 그때 분리하세요)
+    'react-native/no-inline-styles': 'off',
+
+    // 3. 색상 코드 직접 입력: 무조건 허용! (이거 켜면 개발 못합니다)
+    'react-native/no-color-literals': 'off',
+
+    // 4. 안 쓰는 스타일: 이건 켜두는 게 좋습니다. (파일 정리용)
+    'react-native/no-unused-styles': 'warn',
+
+    // 5. 타입스크립트 변수 미사용: '_'로 시작하는 건 봐줌 (ex: _req)
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    // 6. import 정렬: 자동 정렬되므로 켜둡니다.
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
