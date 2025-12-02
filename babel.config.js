c6dcd06bd7cc50d7be5bdd9ae121b1418538c69b
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-12-03 03:15:18 +0900

    chore: 임시로 reanimated 플러그인 제거

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['@emotion/babel-plugin', 'react-native-reanimated/plugin'],
+    plugins: ['@emotion/babel-plugin'],
   };
 };
