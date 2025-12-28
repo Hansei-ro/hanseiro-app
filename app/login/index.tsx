4f2bb52c0b2303e17f1311542dcbed2ac59f8fb5
soyeoung1 <soyeoung06@gmail.com>
2025-12-29 01:38:04 +0900

    design: 레이아웃 구현(#5)

---
+++ app/login/index.tsx
@@ -0,0 +1,3 @@
+import { LoginScreen } from '@/features/login/screens/LoginScreen';
+
+export default LoginScreen;
--- src/features/login/screens/LoginScreen.tsx
+++ src/features/login/screens/LoginScreen.tsx
@@ -2,33 +2,34 @@ import styled from '@emotion/native';
 import React from 'react';
 import { View } from 'react-native';
 
+import { AppleLoginButton } from '../components/AppleLoginButton';
 import { GoogleLoginButton } from '../components/GoogleLoginButton';
 
 export function LoginScreen() {
-  const handleGoogleLogin = () => {};
+  const handleGoogleLogin = () => {
+    console.log('Google 로그인');
+  };
 
+  const handleAppleLogin = () => {
+    console.log('Apple 로그인');
+  };
   return (
     <Container>
-      {/* <LogoPlaceholder /> */}
-      <GoogleLoginButton
-        onPress={handleGoogleLogin}
-        style={{
-          position: 'absolute',
-          top: 467,
-        }}
-      />
+      <ButtonContainer>
+        <GoogleLoginButton onPress={handleGoogleLogin} />
+        <AppleLoginButton onPress={handleAppleLogin} />
+      </ButtonContainer>
     </Container>
   );
 }
 
 const Container = styled(View)`
   flex: 1;
-  justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.primary.white};
 `;
 
-// const LogoPlaceholder = styled(View)`
-//   width: 200px;
-//   height: 200px;
-// `;
+const ButtonContainer = styled(View)`
+  margin-top: 467px;
+  gap: 10px;
+`;
