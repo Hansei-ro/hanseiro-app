24ea6836fdf4a30da6c88bfc6c248cd5ce7b1281
soyeoung1 <soyeoung06@gmail.com>
2025-12-29 22:13:58 +0900

    design: 화면 레이아웃 구현(#29)

---
+++ src/features/profile/screens/DeleteAccountScreen.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/native';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { DeleteInfo } from '../components/DeleteInfo';
+
+import { Button } from '@/shared/ui/Button';
+import { StackHeader } from '@/shared/ui/StackHeader';
+
+export function DeleteAccountScreen() {
+  return (
+    <Container edges={['top']}>
+      <StackHeader title="회원탈퇴" titleAlign="left" />
+      <ContentWrapper>
+        <DeleteInfo />
+        <ButtonContainer>
+          <Button title="회원탈퇴" onPress={() => {}} variant="primary" />
+        </ButtonContainer>
+      </ContentWrapper>
+    </Container>
+  );
+}
+
+const Container = styled(SafeAreaView)`
+  flex: 1;
+  gap: 20px;
+  background-color: ${({ theme }) => theme.colors.primary.white};
+`;
+
+const ContentWrapper = styled(View)`
+  gap: 60px;
+  padding: 0 20px;
+`;
+
+const ButtonContainer = styled(View)``;
