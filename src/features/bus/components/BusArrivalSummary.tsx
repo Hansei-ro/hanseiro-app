fd5d7bbb45f16e4d3b9ec7a08ed96a4d8362ac4e
soyeoung1 <soyeoung06@gmail.com>
2026-01-05 23:23:28 +0900

    chore: develop 최신 반영(#35)

--- src/features/bus/components/BusArrivalSummary.tsx
+++ src/features/bus/components/BusArrivalSummary.tsx
@@ -34,7 +34,7 @@ const BUS_ARRIVAL_MOCK_DATA: BusData[] = [
   },
 ];
 
-export function BusArrivalSummaryBox() {
+export function BusArrivalSummary() {
   return (
     <BusDetailBoxFrame>
       <MoreInformBox>
